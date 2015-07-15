200196b3ea65ee0688c44e9d60ac109d1c6f35e8
endows <you@example.com>
2015-07-15 12:06:04 +0900

    完了

--- client/views/home.js
+++ client/views/home.js
@@ -1,11 +1,5 @@
 Template.home.helpers({
-  myAppVariable: function() {
-    return Session.get('myAppVariable');
-  }
 });
 
 Template.home.events({
-  'click button': function(event, template) {
-    Session.set('myAppVariable', Math.floor(Math.random() * 11));
-  }
 });
--- client/views/school_list.js
+++ client/views/school_list.js
@@ -1,7 +1,15 @@
 Template.school_listView.helpers({
-
+  schools:function(){
+    return School.find()
+  }
 });
 
 Template.school_listView.events({
+  'submit #schoolForm':function(e){
+    e.preventDefault()
+    var input = e.target[0]
+    School.insert({name:input.value})
+    input.value = ''
+  }
 
 });
